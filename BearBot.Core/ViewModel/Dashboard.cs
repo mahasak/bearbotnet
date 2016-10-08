c95aea5d0affbea26ad8d958c2ca228bd1a5f3b8
Mahasak Pijittum <mahasak.pijittum@agoda.com>
2016-10-08 15:44:51 +0700

    Setup project structure

---
+++ BearBot.Core/ViewModel/Dashboard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BearBot.Core.ViewModel
+{
+    class Dashboard
+    {
+        public int TotalScenarios { get; set; }
+        public int CurrentScenario { get; set; }
+        public DateTime RunTime { get; set; }
+        public List<Scenario> Scenarios { get; set; }
+    }
+}
---
+++ BearBot.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BearBot.Web.MvcApplication" Language="C#" %>
